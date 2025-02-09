4ed5f8fa02b788bf400e4d4218b88a1dfb65fa7f
harshmauny <harsh.mauny@gmail.com>
2025-02-09 00:23:18 -0500

    auth Provider added to app

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router } from 'react-router-dom'
 import Main from './Main'
 import { HeroUIProvider } from '@heroui/react'
+import { AuthProvider } from './Context/AuthContext'
 
 function App() {
   return (
     <HeroUIProvider>
       <Router>
-        <Main />
+        <AuthProvider>
+          <Main />
+        </AuthProvider>
       </Router>
     </HeroUIProvider>
   )
