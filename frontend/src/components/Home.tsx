fa51ac864d504b2a6686eabe0f461924d1af0a4c
harshmauny <harsh.mauny@gmail.com>
2025-02-09 00:34:59 -0500

    minor changes

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
 import Header from './Common/Header'
+import { useAuth } from './Context/AuthContext'
 
 function Home() {
+  const { user } = useAuth()
   return (
     <div>
       <Header />
+      {user?.userType === 'company' ? <p>Company</p> : <p>Investor</p>}
     </div>
   )
 }
--- frontend/src/components/Main.tsx
+++ frontend/src/components/Main.tsx
@@ -3,6 +3,7 @@ import { Route, Routes, Navigate } from 'react-router-dom'
 import Login from './Login'
 import Register from './Register'
 import Home from './Home'
+import { useAuth } from './Context/AuthContext'
 
 const Loading = () => (
   <div className="flex justify-center items-center h-screen">
@@ -11,11 +12,18 @@ const Loading = () => (
 )
 
 function Main() {
+  const { isAuthenticated } = useAuth()
   return (
     <Suspense fallback={<Loading />}>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route
+          path="/login"
+          element={!isAuthenticated ? <Login /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/register"
+          element={!isAuthenticated ? <Register /> : <Navigate to="/" />}
+        />
         <Route path="/" element={<Home />} />
       </Routes>
     </Suspense>
