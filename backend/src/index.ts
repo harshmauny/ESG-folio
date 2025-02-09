00594ffbbfd89cc598b0f4054316de4ebfc6bb81
Venkata Sai Praveen Gunda <vgunda@ncsu.edu>
2025-02-09 02:26:23 -0500

    Added Routes in index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,10 @@ import express, { response } from 'express';
 import bodyParser from 'body-parser';
 import { connectDB } from './db/connection';
 import authRoutes from './routes/authRoutes';
+import companyRoutes from './routes/company.route';
+import auditorRoutes from './routes/auditor.route';
 import { verifyToken } from './utils/utils';
+
 
 const app = express();
 const PORT = 3000;
@@ -11,11 +14,14 @@ const PORT = 3000;
 // Middleware
 app.use(bodyParser.json());
 app.use('/api/auth', authRoutes);
+app.use('/api/company', companyRoutes);
+app.use('/api/auditor', auditorRoutes);
 
 app.use(verifyToken);
 
 // Routes
 /* app.use('/api/auth', authRoutes);
+
  */
 // Start the server
 app.listen(PORT, () => {
