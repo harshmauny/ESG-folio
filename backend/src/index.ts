32506c2ccc65b17ddd176b66d7481279207c65bb
sudama <sudama@ncsu.edu>
2025-02-08 22:42:01 -0500

    backend intialise done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,21 @@
-const greet = (name: string): string => {
-  return `Hello, ${name}!`;
-};
 
-console.log(greet("Harsh"));
+import express, { response } from 'express';
+import bodyParser from 'body-parser';
+import { connectDB } from './db/connection';
+import authRoutes from './routes/authRoutes';
+
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(bodyParser.json());
+app.use('/api/auth', authRoutes);
+
+// Routes
+/* app.use('/api/auth', authRoutes);
+ */
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
